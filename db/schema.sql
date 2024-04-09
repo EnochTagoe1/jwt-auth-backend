424c164e4f8e54ae3b374942d681b64ad6fef201
enochtagoe <enochtagoe@pursuit.org>
2024-04-09 13:45:11 -0400

    updated schemaseed

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,51 @@ CREATE DATABASE jwt_auth;
 
 \c jwt_auth
 
+DROP TABLE IF EXISTS USERS;
+
+-- CREATE TABLE USERS;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    location VARCHAR(255),
     email VARCHAR(255) UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    member_since TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS CARS 
+
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    user_id integer REFERENCES users(id),
+    make VARCHAR NOT NULL,
+    model VARCHAR NOT NULL,
+    make_year DATE NOT NULL,
+    color VARCHAR,
+    cylinders integer NOT NULL,
+    price integer NOT NULL,
+    image_path NULL,
+    is_favorite boolean
+
+);
+
+-- DROP TABLE IF EXISTS SHOWS
+
+-- CREATE TABLE shows {
+--     id SERIAL PRIMARY KEY
+--     show_date DATE
+--     location VARCHAR 
+--     PRICE integer
+--     will_attend boolean
+-- }
+
+
+
+
+
 
 
--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,32 @@
 -- db/seed.sql
 \c jwt_auth
 
-INSERT INTO users (username, password_hash, email, created_at, updated_at)
+INSERT INTO users (username, id, first_name, last_name, location, email, password_hash, created_at, member_since)
 VALUES 
 ('demo', '$2b$10$.z68x3792U9LyBwmghfsKexstMO7i0SeNCoDmeJa7bEFPQBnZU3bK', 'demo@example.com', NOW(), NOW());
+('user1', '1000', 'Mike', 'Brown', 'New York, NY', 'mb@email.com', '$2b$10$VKnIRLZSh8QBrEuoZneL6eyzl57gewLQJS.jUv9E7OLlpGxWdwQ0q', NOW(), NOW()),
 
+('carfanatic', '1001', 'John', 'Doe', 'Newark, NJ', 'jdoe@email.com', '$2b$10$VKnIRLZSh8QBrEuoZneL6eyzl57gewLQJS.jUv9E7OLlpGxWdwQ0q', NOW(), NOW()),
+
+('coolride', '1002', 'Jane', 'Doe', 'Atlanta, GA', 'janedoe@email.com', '$2b$10$VKnIRLZSh8QBrEuoZneL6eyzl57gewLQJS.jUv9E7OLlpGxWdwQ0q', NOW(), NOW())
+
+--db seed for cars--
+
+INSERT INTO cars (id, user_id, make, model, make_year, color, cylinders, price, image_path, is_favorite)--img??--
+VALUES 
+('2jue9377', 1, 'CHEVY', 'CORVETTE', '1957', 'RED', '8CYL','90000','image', 'TRUE')--id*/img-frontend??--
+
+--seed for shows--
+-- INSERT INTO shows (id, show_date, location, PRICE, will_attend)
+-- VALUES
+-- ('8087', '2024-04-28', 'Javits Center-New York, NY', '35', 'true')--javts?-- 
+-- ('2448', '2024-06-22', 'Richmond, VA', '$25', 'false')--$
+
+-- CREATE TABLE shows {
+--     id SERIAL PRIMARY KEY
+--     show_date DATE
+--     location VARCHAR 
+--     PRICE integer
+--     will_attend boolean
+-- }
+--1)demo user credentials/hash 2)npm i dependencies? 3)seed user pasword-hash correct?? 4).env here or front?? 5)user.js-CreateUser edits 6)when db:init db:seed??
